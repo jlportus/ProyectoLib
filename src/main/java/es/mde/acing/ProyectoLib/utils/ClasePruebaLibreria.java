b3dd3d30958e1dabaf4fd8d135d2ac4918b93ad2
jlPortus <jl_puerto@hotmail.com>
2023-03-01 21:41:50 +0100

    probando comentarios

--- src/main/java/es/mde/acing/ProyectoLib/utils/ClasePruebaLibreria.java
+++ src/main/java/es/mde/acing/ProyectoLib/utils/ClasePruebaLibreria.java
@@ -2,6 +2,11 @@ package es.mde.acing.ProyectoLib.utils;
 
 public class ClasePruebaLibreria {
 
+	/*
+	 * metodo de prueba que permite comprobar que la libreria esta enlazada
+	 * 
+	 * @author jlportus
+	 */
 	public static void holaLibreria() {
 		System.out.println("Hola desde la libreria");
 	}
