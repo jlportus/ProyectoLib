d76e18fa76ae2ee90587d7ae6b16b45ccdd75c8d
jlPortus <jl_puerto@hotmail.com>
2023-03-01 20:17:18 +0100

    Version inicial de mi libreria

---
+++ src/main/java/es/mde/acing/ProyectoLib/ProyectoLibApplication.java
@@ -0,0 +1,10 @@
+package es.mde.acing.ProyectoLib;
+
+
+public class ProyectoLibApplication {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/es/mde/acing/ProyectoLib/utils/ClasePruebaLibreria.java
@@ -0,0 +1,8 @@
+package es.mde.acing.ProyectoLib.utils;
+
+public class ClasePruebaLibreria {
+
+	public static void holaLibreria() {
+		System.out.println("Hola desde la libreria");
+	}
+}
